ba7d7490d07d88157491f8c34eeb66270104cd93
JSer <56359827+JSerWP@users.noreply.github.com>
2024-05-23 01:44:44 +0800

    Add files via upload

---
+++ model/README.md
@@ -0,0 +1,74 @@
+# SegVol: Universal and Interactive Volumetric Medical Image Segmentation
+This repo is the official implementation of [SegVol: Universal and Interactive Volumetric Medical Image Segmentation](https://arxiv.org/abs/2311.13385).
+
+## News🚀
+(2023.11.24) *You can download weight files of SegVol and ViT(CTs pre-train) [here](https://drive.google.com/drive/folders/1TEJtgctH534Ko5r4i79usJvqmXVuLf54?usp=drive_link).* 🔥
+
+(2023.11.23) *The brief introduction and instruction have been uploaded.*
+
+(2023.11.23) *The inference demo code has been uploaded.*
+
+(2023.11.22) *The first edition of our paper has been uploaded to arXiv.* 📃
+
+## Introduction
+<img src="https://github.com/BAAI-DCAI/SegVol/blob/main/asset/overview.png" width="60%" height="60%">
+
+The SegVol is a universal and interactive model for volumetric medical image segmentation. SegVol accepts **point**, **box** and **text** prompt while output volumetric segmentation. By training on 90k unlabeled Computed Tomography (CT) volumes and 6k labeled CTs, this foundation model supports the segmentation of over 200 anatomical categories.
+
+We will release SegVol's **inference code**, **training code**, **model params** and **ViT pre-training params** (pre-training is performed over 2,000 epochs on 96k  CTs). 
+
+## Usage
+### Requirements
+The [pytorch v1.11.0](https://pytorch.org/get-started/previous-versions/) (or higher virsion) is needed first. Following install key requirements using commands:
+
+```
+pip install 'monai[all]==0.9.0'
+pip install einops==0.6.1
+pip install transformers==4.18.0
+pip install matplotlib
+``` 
+### Config and run demo script
+1. You can download the demo case [here](https://drive.google.com/drive/folders/1TEJtgctH534Ko5r4i79usJvqmXVuLf54?usp=drive_link), or download the whole demo dataset  [AbdomenCT-1K](https://github.com/JunMa11/AbdomenCT-1K) and choose any demo case you want.
+2. Please set CT path and Ground Truth path of the case in the [config_demo.json](https://github.com/BAAI-DCAI/SegVol/blob/main/config/config_demo.json).
+3. After that, config the [inference_demo.sh](https://github.com/BAAI-DCAI/SegVol/blob/main/script/inference_demo.sh) file for execution:
+
+    - `$segvol_ckpt`: the path of SegVol's checkpoint (Download from [here](https://drive.google.com/drive/folders/1TEJtgctH534Ko5r4i79usJvqmXVuLf54?usp=drive_link)).
+
+    - `$work_dir`: any path of folder you want to save the log files and visualizaion results.
+
+4. Finally, you can control the **prompt type**, **zoom-in-zoom-out mechanism** and **visualizaion switch** [here](https://github.com/BAAI-DCAI/SegVol/blob/35f3ff9c943a74f630e6948051a1fe21aaba91bc/inference_demo.py#L208C11-L208C11).
+5. Now, just run `bash script/inference_demo.sh` to infer your demo case.
+
+## Citation
+If you find this repository helpful, please consider citing:
+```
+@misc{du2023segvol,
+      title={SegVol: Universal and Interactive Volumetric Medical Image Segmentation}, 
+      author={Yuxin Du and Fan Bai and Tiejun Huang and Bo Zhao},
+      year={2023},
+      eprint={2311.13385},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
+
+## Acknowledgement
+Thanks for the following amazing works:
+
+[HuggingFace](https://huggingface.co/).
+
+[CLIP](https://github.com/openai/CLIP).
+
+[MONAI](https://github.com/Project-MONAI/MONAI).
+
+[Image by brgfx](https://www.freepik.com/free-vector/anatomical-structure-human-bodies_26353260.htm) on Freepik.
+
+[Image by muammark](https://www.freepik.com/free-vector/people-icon-collection_1157380.htm#query=user&position=2&from_view=search&track=sph) on Freepik.
+
+[Image by pch.vector](https://www.freepik.com/free-vector/different-phone-hand-gestures-set_9649376.htm#query=Vector%20touch%20screen%20hand%20gestures&position=4&from_view=search&track=ais) on Freepik.
+
+[Image by starline](https://www.freepik.com/free-vector/set-three-light-bulb-represent-effective-business-idea-concept_37588597.htm#query=idea&position=0&from_view=search&track=sph) on Freepik.
+
+
+
+
---
+++ model/inference_cpu.py
@@ -0,0 +1,177 @@
+import argparse
+import os
+import torch
+import torch.nn.functional as F
+import json
+import monai.transforms as transforms
+
+from model.segment_anything_volumetric import sam_model_registry
+from model.network.model import SegVol
+from model.data_process.demo_data_process import process_ct_gt
+from model.utils.monai_inferers_utils import sliding_window_inference, generate_box, select_points, build_binary_cube, build_binary_points, logits2roi_coor
+from model.utils.visualize import draw_result
+import streamlit as st
+
+def set_parse():
+    # %% set up parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_mode", default=True, type=bool)
+    parser.add_argument("--resume", type = str, default = 'SegVol_v1.pth')
+    parser.add_argument("-infer_overlap", default=0.0, type=float, help="sliding window inference overlap")
+    parser.add_argument("-spatial_size", default=(32, 256, 256), type=tuple)
+    parser.add_argument("-patch_size", default=(4, 16, 16), type=tuple)
+    parser.add_argument('-work_dir', type=str, default='./work_dir')
+    ### demo
+    parser.add_argument("--clip_ckpt", type = str, default = 'model/config/clip')
+    args = parser.parse_args()
+    return args
+
+def zoom_in_zoom_out(args, segvol_model, image, image_resize, text_prompt, point_prompt, box_prompt):
+    image_single_resize = image_resize
+    image_single = image[0,0]
+    ori_shape = image_single.shape
+    resize_shape = image_single_resize.shape[2:]
+    
+    # generate prompts
+    text_single = None if text_prompt is None else [text_prompt]
+    points_single = None
+    box_single = None
+
+    if args.use_point_prompt:
+        point, point_label = point_prompt
+        points_single = (point.unsqueeze(0).float(), point_label.unsqueeze(0).float()) 
+        binary_points_resize = build_binary_points(point, point_label, resize_shape)
+    if args.use_box_prompt:
+        box_single = box_prompt.unsqueeze(0).float()
+        binary_cube_resize = build_binary_cube(box_single, binary_cube_shape=resize_shape)
+    
+    ####################
+    # zoom-out inference:
+    print('--- zoom out inference ---')
+    print(text_single)
+    print(f'use text-prompt [{text_single!=None}], use box-prompt [{box_single!=None}], use point-prompt [{points_single!=None}]')
+    with torch.no_grad():
+        logits_global_single = segvol_model(image_single_resize,
+                                            text=text_single, 
+                                            boxes=box_single, 
+                                            points=points_single)
+    # for key in score.keys():
+    #     if key == 'res':
+    #         print(f'The mask is {score[key]}')
+    #         st.session_state.semantic_res = score[key]
+    #     else:
+    #         print(f'{key} scored {score[key]:.4f}')
+    # resize back global logits
+    logits_global_single = F.interpolate(
+            logits_global_single.cpu(),
+            size=ori_shape, mode='nearest')[0][0]
+    
+    # build prompt reflection for zoom-in
+    if args.use_point_prompt:
+        binary_points = F.interpolate(
+            binary_points_resize.unsqueeze(0).unsqueeze(0).float(),
+            size=ori_shape, mode='nearest')[0][0]
+    if args.use_box_prompt:
+        binary_cube = F.interpolate(
+            binary_cube_resize.unsqueeze(0).unsqueeze(0).float(),
+            size=ori_shape, mode='nearest')[0][0]
+    # draw_result('unknow', image_single_resize, None, point_prompt, logits_global_single, logits_global_single)
+    if not args.use_zoom_in:
+        return logits_global_single
+
+    ####################
+    # zoom-in inference:
+    min_d, min_h, min_w, max_d, max_h, max_w = logits2roi_coor(args.spatial_size, logits_global_single)
+    if min_d is None:
+        print('Fail to detect foreground!')
+        return logits_global_single
+
+    # Crop roi
+    image_single_cropped = image_single[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1].unsqueeze(0).unsqueeze(0)
+    global_preds = (torch.sigmoid(logits_global_single[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1])>0.5).long()
+    
+    assert not (args.use_box_prompt and args.use_point_prompt)
+    # label_single_cropped = label_single[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1].unsqueeze(0).unsqueeze(0)
+    prompt_reflection = None
+    if args.use_box_prompt:
+        binary_cube_cropped = binary_cube[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1]
+        prompt_reflection = (
+            binary_cube_cropped.unsqueeze(0).unsqueeze(0),
+            global_preds.unsqueeze(0).unsqueeze(0)
+        )
+    if args.use_point_prompt:
+        binary_points_cropped = binary_points[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1]
+        prompt_reflection = (
+            binary_points_cropped.unsqueeze(0).unsqueeze(0),
+            global_preds.unsqueeze(0).unsqueeze(0)
+        )
+
+    ## inference
+    with torch.no_grad():
+        logits_single_cropped = sliding_window_inference(
+                image_single_cropped, prompt_reflection,
+                args.spatial_size, 1, segvol_model, args.infer_overlap,
+                text=text_single,
+                use_box=args.use_box_prompt,
+                use_point=args.use_point_prompt,
+                logits_global_single=logits_global_single,
+            )
+        logits_single_cropped = logits_single_cropped.cpu().squeeze()
+        if logits_single_cropped.shape != logits_global_single.shape:
+            logits_global_single[min_d:max_d+1, min_h:max_h+1, min_w:max_w+1] = logits_single_cropped
+
+    return logits_global_single
+
+@st.cache_resource
+def build_model():
+    # build model
+    st.write('building model')
+    clip_ckpt = 'model/config/clip'
+    resume = 'SegVol_v1.pth'
+    sam_model = sam_model_registry['vit']()
+    segvol_model = SegVol(
+                        image_encoder=sam_model.image_encoder, 
+                        mask_decoder=sam_model.mask_decoder,
+                        prompt_encoder=sam_model.prompt_encoder,
+                        clip_ckpt=clip_ckpt,
+                        roi_size=(32,256,256),
+                        patch_size=(4,16,16),
+                        test_mode=True,
+                        )
+    segvol_model = torch.nn.DataParallel(segvol_model)
+    segvol_model.eval()
+    # load param
+    if os.path.isfile(resume):
+        ## Map model to be loaded to specified single GPU
+        loc = 'cpu'
+        checkpoint = torch.load(resume, map_location=loc)
+        segvol_model.load_state_dict(checkpoint['model'], strict=True)
+        print("loaded checkpoint '{}' (epoch {})".format(resume, checkpoint['epoch']))
+    print('model build done!')
+    return segvol_model
+
+@st.cache_data
+def inference_case(_image, _image_zoom_out, _point_prompt, text_prompt, _box_prompt):
+    # seg config
+    args = set_parse()
+    args.use_zoom_in = False
+    args.use_text_prompt = text_prompt is not None
+    args.use_box_prompt = _box_prompt is not None
+    args.use_point_prompt = _point_prompt is not None
+
+    segvol_model = build_model()
+
+    # run inference
+    logits = zoom_in_zoom_out(
+        args, segvol_model, 
+        _image.unsqueeze(0), _image_zoom_out.unsqueeze(0), 
+        text_prompt, _point_prompt, _box_prompt)
+    print(logits.shape)
+    resize_transform = transforms.Compose([
+        transforms.AddChannel(),
+        transforms.Resize((325,325,325), mode='trilinear')
+    ]
+    )
+    logits_resize = resize_transform(logits)[0]
+    return (torch.sigmoid(logits_resize) > 0.5).int().numpy(), (torch.sigmoid(logits) > 0.5).int().numpy()
+    
